d695ad4eeabe6491eccd81ec21d7906dea64b26d
KevinPatel197 <kevinpatel6221@gmail.com>
2024-01-27 13:23:53 +0530

    change2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// FETCH DATA FROM SERVER
+
+export async function fetchData(url, successCallback, errorCallback) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        successCallback(data);
+    } else {
+        const error = await response.json();
+        errorCallback && errorCallback(error);
+    }
+}
